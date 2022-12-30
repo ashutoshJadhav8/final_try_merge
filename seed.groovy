eb690809aee8723a8158e684c658a6706954bcd6
ashutoshJadhav8 <102962428+ashutoshJadhav8@users.noreply.github.com>
2022-12-30 11:35:38 +0530

    Update seed.groovy

--- seed.groovy
+++ seed.groovy
@@ -1,23 +1,28 @@
-unclassified:  
-  globalLibraries:
-    libraries:
-      - defaultVersion: "master"
-        name: "strimzi-ci"
-        retriever:
-          modernSCM:
-            scm:
-              git:
-                remote: "https://github.com/ashutoshJadhav8/final_try_merge.git"
-                traits:
-                  - "gitBranchDiscovery"
-jobs:
-  - script: >
-      pipelineJob('seed') {
-        definition {
-          cps {
-            sandbox(true)
-            scrip("seed()")
-          }
-
+node {
+    stage('seed job checkout') {
+        git url: 'https://github.com/ashutoshJadhav8/demo-JCASC-GIT.git', branch: 'main'
+    }
+    stage('pipelineMap jobs') {
+        jobList = sh (script: """ git ls-tree --full-tree -r --name-only HEAD|egrep -iv "default|readme.md|.groovy" | sed -e "s/.yaml//" | egrep
+           """)
+        jobList.each{
+            def folderName = it.split('/')[0]
+            def jobName = it.split('/')[1]
+            log.info "Pipelinemap folderName $(folderName)"
+            log.info "Pipelinemap jobName $(folderJob)"
+            jobDsl scriptText """
+                pipelineJob("${folderName}/${jobName}"){
+                    parameters{
+                        stringParam('app8ranch', 'develop')
+                    }
+                    definition {
+                        cps {
+                            sandbox(true)
+                            script("cicd()")
+                        }
+                    }
+                }
+            """
         }
-      }
+    }
+}
